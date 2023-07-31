60cf41d1ff6836a2556c7a30f6f0c1f5028177ee
lodioso <129630980+lodioso@users.noreply.github.com>
2023-07-31 18:02:28 +0200

    Added the round function for the game.

--- javascript.js
+++ javascript.js
@@ -1,12 +1,52 @@
 function getComputerChoice() {
     const weapon = Math.floor(Math.random() * 3);
     if (weapon == 0) {
-        return "Rock";
-    } else if (weapon == 1) {
-        return "Paper";
+        return "rock";
+    }
+    if (weapon == 1) {
+        return "paper";
     } else {
-        return "Scissor";
+        return "scissor";
     }
 }
 
-console.log(getComputerChoice())
+
+function round(playerSelection, computerSelection) {
+
+    // Win
+    if (playerSelection == "rock" && computerSelection == "scissor") {
+        return "You win! Rock beats Scissor";
+    }
+
+    if (playerSelection == "scissor" && computerSelection == "paper") {
+        return "You win! Scissor beats paper";
+    }
+
+    if (playerSelection == "paper" && computerSelection == "rock") {
+        return "You win! Paper beats rock";
+    }
+    // A tie
+
+    if (playerSelection == computerSelection) {
+        return "It's a tie!";
+    }
+
+    // Lose
+    if (playerSelection == "rock" && computerSelection == "paper") {
+        return "You lose! Paper beats rock";
+    }
+
+    if (playerSelection == "scissor" && computerSelection == "rock") {
+        return "You lose! Rock beats scissor";
+    }
+
+    if (playerSelection == "paper" && computerSelection == "scissor") {
+        return "You lose! Scissor beats paper";
+    }
+
+}
+
+let playerSelection = "rOcK";
+playerSelection = playerSelection.toLowerCase();
+const computerSelection = getComputerChoice();
+console.log(round(playerSelection, computerSelection));
